ce0a59f3af3e93b4c171858184c4ac863919cf21
Thijs Spijker <thijs.spijker@hva.nl>
2020-03-04 16:17:45 +0100

    add defaults for data

--- src/modules/cleaner.mjs
+++ src/modules/cleaner.mjs
@@ -1,12 +1,12 @@
 export default function cleanData(data) {
 	return data.map(item => {
 		return {
-			title: item.titles[0],
-			author: item.authors[0],
-			summary: item.summaries ? item.summaries[0] : 'Geen samenvatting',
-			format: item.formats[0].text,
-			year: parseInt(item.year),
+			title: item.titles[0] || 'Geen titel',
+			author: item.authors[0] || 'Geen auteur',
+			summary: item.summaries || 'Geen samenvatting',
+			format: item.formats[0].text || 'Geen formaat',
+			year: parseInt(item.year) || 'Geen jaar',
 			detailLink: item.detailLink
-		}
+		};
 	});
 }
