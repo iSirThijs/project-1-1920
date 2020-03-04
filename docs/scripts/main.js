a919de4a2972e6d7f095ce6a5e342845e84ca628
Thijs Spijker <thijs.spijker@hva.nl>
2020-03-04 16:17:54 +0100

    build

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -311,13 +311,13 @@
   function cleanData(data) {
   	return data.map(item => {
   		return {
-  			title: item.titles[0],
-  			author: item.authors[0],
-  			summary: item.summaries ? item.summaries[0] : 'Geen samenvatting',
-  			format: item.formats[0].text,
-  			year: parseInt(item.year),
+  			title: item.titles[0] || 'Geen titel',
+  			author: item.authors[0] || 'Geen auteur',
+  			summary: item.summaries || 'Geen samenvatting',
+  			format: item.formats[0].text || 'Geen formaat',
+  			year: parseInt(item.year) || 'Geen jaar',
   			detailLink: item.detailLink
-  		}
+  		};
   	});
   }
 
@@ -429,22 +429,76 @@
   	return main;
   };
 
-  function recommendationsPage() {
-  	removeOldPage();
-  	const body = document.body;
-  	body.appendChild(recommendations());
-  }
+  const welcome =
+  	`<h3>Welkom</h3>
+	<p>OBA jouw boek geeft aanbevelingen voor boeken op basis van data die de OBA over jou heeft. Welke data daarvoor gebruikt wordt mag jij bepalen.</p>
+	`;
 
-  function profilePage() {
-  	removeOldPage();
-  	const body = document.body;
-  	body.appendChild(profile());
-  }
+  const user = `
+	<h3>Persoonsgegevens</h3>
+	<p>Persoonsgegevens gaan over wie jij bent, zoals hoe oud je bent of waar je woont</p>
+	<form>
+		<input type="checkbox" id="age" name="age">
+		<label for="age">Leeftijd</label>
+		<input type="checkbox" id="city" name="city">
+		<label for="city">Woonplaats</label>
+		<input type="checkbox" id="postalCode" name="postalCode">
+		<label for="postalcode">Postcode</label>
+		<input type="checkbox" id="gender" name="gender">
+		<label for="gender">geslacht</label>
+	</form>`;
 
-  function removeOldPage() {
-  	const main = document.querySelector('main');
-  	main.remove();
-  }
+  const loan = `
+	<h3>Leengeschiedenis</h3>
+	<p>Leengeschiedenis gaat over wat je bij de OBA hebt geleend, zoals het soort boek of welke auteurs je leest</p>
+	<form>
+		<input type="checkbox" id="genres" name="genres">
+		<label for="genres">Genres</label>
+		<input type="checkbox" id="obaLocation" name="obaLocation">
+		<label for="obaLocation">OBA locaties</label>
+		<input type="checkbox" id="mediaType" name="mediaType">
+		<label for="mediaType">Media type</label>
+		<input type="checkbox" id="loanCategory" name="loanCategory">
+		<label for="loanCategory">Categorie</label>
+	</form>
+	`;
+
+  // export default function(data){
+  // 	const title = `<h3>${data.title}</h3>`
+  // 	const description = `<p>${data.description}</p>'
+  // 	<form>
+  // 		<input type="checkbox" id="${id}" name="${id}">
+  // 		<label for="genres">Genres</label>
+  // 		<input type="checkbox" id="obaLocation" name="obaLocation">
+  // 		<label for="obaLocation">OBA locaties</label>
+  // 		<input type="checkbox" id="mediaType" name="mediaType">
+  // 		<label for="mediaType">Media type</label>
+  // 		<input type="checkbox" id="loanCategory" name="loanCategory">
+  // 		<label for="loanCategory">Categorie</label>
+  // 	</form>
+  // 	`;
+  // }
+
+  var step = /*#__PURE__*/Object.freeze({
+    __proto__: null,
+    welcome: welcome,
+    user: user,
+    loan: loan
+  });
+
+  var fakeUserImport = fakeUser => {
+  	return {
+  		userID: 83913,
+  		age: 43,
+  		city: 'Amsterdam',
+  		postalCode: 1022,
+  		gender: 'female',
+  		genres: [['psychologische roman', 1], ['horror', 3], ['biografie', 4], ['stripverhaal', 2], ['detectiveroman', 4]],
+  		obaLocation: [['CEN', 10], ['BVD', 4]],
+  		mediaType: [['NF', 7], ['JROM', 2], ['ROM', 1], ['DVDSPM', 4] ],
+  		loanCategory: [['VOLWS', 14]]
+  	}
+  };
 
   function setEmptyUser() {
   const emptyUser = {
@@ -463,17 +517,132 @@
   	return emptyUser;
   }
 
+
+  function updateProfile(key, checked) {
+  	const fakeUser = fakeUserImport();
+  	const user = getStoredData('user');
+  	const emptyUser = {
+  		userID: 83913,
+  		age: null,
+  		city: null,
+  		postalCode: null,
+  		gender: null,
+  		genres: [],
+  		obaLocation: [],
+  		mediaType: [],
+  		loanCategory: []
+  	};
+
+  	user[key] = checked ? fakeUser[key] : emptyUser[key];
+
+  	storeData('user', user);
+
+  }
+
+  var setup = (nextStep) => {
+  	// console.log('Setup Page');
+  	if(nextStep === 'welcome') setEmptyUser();
+  	const main = document.createElement('main');
+  	const section = createSetupStep(nextStep);
+
+  	main.appendChild(section);
+
+  	return main;
+  };
+
+  function createSetupStep(nextStep) {
+  	const user = getStoredData('user');
+
+  	const section = document.createElement('section');
+  	section.insertAdjacentHTML('beforeend', step[nextStep]);
+
+  	const links = createLinks(nextStep);
+  	section.appendChild(links);
+
+  	const checkboxes = section.querySelectorAll('input[type="checkbox"]');
+  	// console.log(checkboxes);
+  	checkboxes.forEach((checkbox) => {
+  		let checkboxID = checkbox.getAttribute('id');
+
+  		if ( Array.isArray(user[checkboxID]) && user[checkboxID].length > 0) checkbox.checked = true;
+  		else if(!Array.isArray(user[checkboxID]) && user[checkboxID]) checkbox.checked = true;
+  		else checkbox.checked = false;
+  		
+
+  		checkbox.addEventListener('change', (event) => {
+  			const key = event.target.name;
+  			const checked = event.target.checked;
+
+  			updateProfile(key, checked);
+  		});
+  	});
+  	
+  	return section;
+  }
+
+
+  function createLinks(nextStep){
+  	const div = document.createElement('div');
+
+  	switch(nextStep){
+  	case 'welcome' : {
+  		div.insertAdjacentHTML('beforeend','<a href=\'#setup/user\'>Volgende</a>');
+  		break;
+  	}
+  	case 'user' : {
+  		div.insertAdjacentHTML('beforeend','<a href=\'#setup/welcome\'>Vorige</a><a href=\'#setup/loan\'>Volgende</a>');
+  		break;
+  	}
+  	case 'loan' : {
+  		div.insertAdjacentHTML('beforeend','<a href=\'#setup/user\'>Vorige</a><a href=\'#profile\'>Ga naar profiel</a><a href=\'#profile\'>Ga naar aanbevelingen</a>');
+  		break;
+  	}
+  	}
+  	return div;
+  	
+  }
+
+  //<div><button>Vorige</button><button>Volgende</button></div>
+
+  function recommendationsPage() {
+  	removeOldPage();
+  	const body = document.body;
+  	body.appendChild(recommendations());
+  }
+
+  function profilePage() {
+  	removeOldPage();
+  	const body = document.body;
+  	body.appendChild(profile());
+  }
+
+  function removeOldPage() {
+  	const main = document.querySelector('main');
+  	main.remove();
+  }
+
+  function setupPage(step) {
+  	removeOldPage();
+  	const body = document.body;
+  	body.appendChild(setup(step));
+  }
+
   routie({
   	'': init,
   	'profile': profilePage,
-  	'recommendations': recommendationsPage
+  	'recommendations': recommendationsPage,
+  	'setup': () => routie('setup/welcome'),
+  	'setup/:step': setupPage
   });
 
+  function init(){
+  	// temp clear of user data
+  	localStorage.removeItem('user');
 
-  function init() {
-  	setEmptyUser();
-  	routie('profile');
-  }
+  	if (checkLocalStorage()) routie('profile');
+  	else {
+  		routie('setup');
+  	}}
 
 })));
 //# sourceMappingURL=main.js.map
--- src/user_profile.js
+++ src/user_profile.js
@@ -5,7 +5,7 @@ export default fakeUser => {
 		city: 'Amsterdam',
 		postalCode: 1022,
 		gender: 'female',
-		genres: [['psychologische roman', 1], ['thriller', 3], ['biografie', 4], ['stripverhaal', 2], ['detectiveroman', 4]],
+		genres: [['psychologische roman', 1], ['horror', 3], ['biografie', 4], ['stripverhaal', 2], ['detectiveroman', 4]],
 		obaLocation: [['CEN', 10], ['BVD', 4]],
 		mediaType: [['NF', 7], ['JROM', 2], ['ROM', 1], ['DVDSPM', 4] ],
 		loanCategory: [['VOLWS', 14]]
